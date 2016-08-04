a4068e9e8cf7d7cb43d089cc5fb5a02586fc66e8
RupalSanghavi <rsanghavi@smu.edu>
2016-08-04 10:27:13 -0500

    added all files needed for subscriber

---
+++ src/main/java/Constants.java
@@ -0,0 +1,21 @@
+
+/**
+ * A class holds constants.
+ */
+public final class Constants {
+    /**
+     * A memcache key for storing query result for recent messages.
+     */
+    public static final String MESSAGE_CACHE_KEY = "messageCache";
+    /**
+     * A base package name.
+     */
+    public static final String BASE_PACKAGE =
+            "com.homedepot.sa.el.common.foundation.registry";
+
+    /**
+     * Prevents instantiation.
+     */
+    private Constants() {
+    }
+}
